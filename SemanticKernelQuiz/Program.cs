b49edb1df1049cc660cc4b16e63a728b94127b82
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-01-29 16:34:02 +0300

    Add project files.

---
+++ SemanticKernelQuiz/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//GPT 4 min does not do a good count
+//string modelId = "gpt-4o-mini"; 
+using Microsoft.Extensions.AI;
+using OpenAI;
+using SemanticKernelQuiz;
+
+string modelId = "gpt-4o";
+string OpenAiKey = Environment.GetEnvironmentVariable("OpenAiTestKey");
+
+
+var courseInfo = System.IO.File.ReadAllText("CourseInfo.txt");
+
+var prompt = System.IO.File.ReadAllText("Prompt.txt");
+
+var FullPrompt =   prompt.Replace("$CourseInfo", courseInfo);
+
+var client = new OpenAIClient(new System.ClientModel.ApiKeyCredential(OpenAiKey));
+
+var ChatClient= new OpenAIChatClient(client, modelId)
+    .AsBuilder()
+    .UseFunctionInvocation()
+    .Build();
+
+var Message = new ChatMessage(ChatRole.User, FullPrompt);
+//var Result=await ChatClient.CompleteAsync<Quiz>(new List<ChatMessage>() { Message }); 
+
+var Result = await ChatClient.CompleteAsync<List<QuizQuestion>>(new List<ChatMessage>() { Message });
+Console.WriteLine("Hello, World!");
---
+++ SemanticKernelQuiz/Quiz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SemanticKernelQuiz
+{
+    public class QuizQuestion
+    {
+        [JsonPropertyName("question")]
+        [Description("The question to ask the user")]
+        public string Question { get; set; }
+        [JsonPropertyName("answer")]
+        [Description("The answer to the question")]
+        public string Answer { get; set; }
+        [JsonPropertyName("options")]
+        [Description("The options to choose from")]
+       public List<string> Options { get; set; }
+
+        [JsonPropertyName("weight")]
+        [Description("the evaluation weight of the question")]
+        public double Weight { get; set; }
+    }
+   
+    public class Quiz
+    {
+        [JsonPropertyName("subject")]
+        [Description("The subject of the quiz base on the topic to study")]
+        public string Subject { get; set; }
+
+        [JsonPropertyName("questions")]
+        List<QuizQuestion> Questions { get; set; }
+    }
+}
