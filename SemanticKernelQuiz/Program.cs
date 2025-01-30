b478b9edd245c72fe251fe6d5ebd2f750da96581
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-01-30 09:30:58 +0300

    Demo

--- SemanticKernelQuiz/Program.cs
+++ SemanticKernelQuiz/Program.cs
@@ -8,6 +8,15 @@ using SemanticKernelQuiz;
 
 string modelId = "gpt-4o";
 string OpenAiKey = Environment.GetEnvironmentVariable("OpenAiTestKey");
+var client = new OpenAIClient(new System.ClientModel.ApiKeyCredential(OpenAiKey));
+var ChatClient = new OpenAIChatClient(client, modelId)
+    .AsBuilder()
+    .UseFunctionInvocation()
+    .Build();
+
+
+//IChatClient ChatClient =
+//      new OllamaChatClient(new Uri("http://localhost:11434/"), "llama3.2:3b");
 
 
 var courseInfo = System.IO.File.ReadAllText("CourseInfo.txt");
@@ -16,15 +25,14 @@ var prompt = System.IO.File.ReadAllText("Prompt.txt");
 
 var FullPrompt =   prompt.Replace("$CourseInfo", courseInfo);
 
-var client = new OpenAIClient(new System.ClientModel.ApiKeyCredential(OpenAiKey));
-
-var ChatClient= new OpenAIChatClient(client, modelId)
-    .AsBuilder()
-    .UseFunctionInvocation()
-    .Build();
 
 var Message = new ChatMessage(ChatRole.User, FullPrompt);
 //var Result=await ChatClient.CompleteAsync<Quiz>(new List<ChatMessage>() { Message }); 
 
-var Result = await ChatClient.CompleteAsync<List<QuizQuestion>>(new List<ChatMessage>() { Message });
+var ChatCompletion = await ChatClient.CompleteAsync<List<QuizQuestion>>(new List<ChatMessage>() { Message });
+
+//write to json ChatCompletion.Result
+var json = System.Text.Json.JsonSerializer.Serialize(ChatCompletion.Result);
+System.IO.File.WriteAllText("ChatCompletion.json", json);
+
 Console.WriteLine("Hello, World!");
